739d8141d82747cc57671d668761df6e9434498d
Ali <abn@webit4.me>
2014-08-02 18:01:14 +0100

    Adding some test pages

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,11 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+Route::get('users', 'UserController@getIndex');
+
+//Route::get('users', function()
+//{
+//    return 'Users!';
+//});
